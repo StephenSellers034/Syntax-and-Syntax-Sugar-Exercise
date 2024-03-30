0e310de09e5cd24f14b5ad2dd6b54e2029687538
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-03-30 18:54:16 -0400

    Mysugaredits

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9);
+            if (answer < 9)
+            {
+                Console.WriteLine($"{answer} is less than nine");
+            }
+            else
+            {
+                Console.WriteLine($"{answer} is greater than or equal to nine");
+            }
+
+            var message = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine" ;
+            Console.WriteLine(message);
+
         }
     }
 }
